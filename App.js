128dd230063ef0c5c765698bfb7d5f32173a97e2
Rob Jenkins <jenkinsr@mctc.edu>
2022-02-08 22:23:47 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+
+import sunny from './assets/images/sunny.jpg';
+
+import Icon from './weatherIcon';
+import data from './data';
+
+import ForecastHourly from './components/ForecastHourly';
+import CurrentConditions from './components/CurrentConditions';
+import WeatherDetails from './components/WeatherDetails';
+import ForecastSummary from './components/ForecastSummary';
+
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    icomoon: require('./assets/fonts/icomoon.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Image source={sunny} style={styles.image} /> */}
+      <Icon name="wi-night-alt-snow" style={styles.icon} />
+
+      <CurrentConditions current={data.current} />
+      <ForecastSummary />
+
+      <WeatherDetails />
+      <ForecastHourly />
+      
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  icon: {
+    fontSize: 80,
+    color: 'darkorchid'
+  }
 });
